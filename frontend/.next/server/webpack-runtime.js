402d31a61ebe86e5e20b6a356d31d9ae31d47c98
zadekastrati1 <zk69847@ubt-uni.net>
2025-05-26 23:04:52 +0200

    chore: remove node_modules and add gitignore

--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("b241c55b4553037c")
+/******/ 		__webpack_require__.h = () => ("62e04abe62a50699")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
